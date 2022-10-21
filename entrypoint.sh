f892848b5c4c3c466157e49a2158cbbdf7ca9b7f
Richard Frankel <rfrankel@roblox.com>
2022-10-21 00:20:03 -0400

    Re-enable partial workaround inside container.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 # export GIT_CEILING_DIRECTORIES=/__w
-# git config --global --add safe.directory /github/workspace
+git config --global --add safe.directory /github/workspace
 # git config --global --add safe.directory /__w/clang-format-action-1/clang-format-action-1
 
 git clang-format --style="$1" $2
