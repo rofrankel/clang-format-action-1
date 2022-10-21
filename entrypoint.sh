6b7d2907b68871e07116088531d2618956c55856
Richard Frankel <richard@frankel.tv>
2022-10-21 00:39:33 -0400

    Create a test workflow (#1)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh -l
 
+
+# Allow agent-libs repo access, ref: https://github.com/actions/checkout/issues/766
+git config --global --add safe.directory "$GITHUB_WORKSPACE"
+
 git clang-format --style="$1" $2
